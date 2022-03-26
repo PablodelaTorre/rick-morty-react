611b13b2bd37e0a3eb6118a4fefcca75e2320493
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-26 19:05:46 -0300

    avances en el renderizado de las tarjetas

--- src/components/Personaje.js
+++ src/components/Personaje.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const Personaje = ({personaje}) => {
+
+    const {name, species, image, status} = personaje
+
     return (
-        <div className='col.md-4 my-2'>
+        <div className='col-md-4 my-2'>
             <div className='card'>
-                <img src="" alt="" className='card-img-top'/>
+                <img src={image} alt={`imagen-${name}`} className='card-img-top'/>
+                <div className='card-body d-flex flex-column justify-content-center align-items-center'>
+                    <p className='fw-bold fs-3'>{name}</p>
+                    <p className='fw-bold fs-5 text-info'>{species}</p>
+                    <p className='fw-bold fs-5 text-success'>{status}</p>
+                </div>
             </div>
+            
         </div>
     )
 }
